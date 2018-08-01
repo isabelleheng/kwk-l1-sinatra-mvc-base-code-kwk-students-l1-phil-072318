954fbebd01f3ac2f4d7bee0da358cc8e7a78ea50
isabelleheng <isabellejhheng@gmail.com>
2018-08-01 17:05:31 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,27 @@
 require 'bundler'
+require './models/model'
 Bundler.require
 
 class MyApp < Sinatra::Base
-
-  get '/' do
-    erb :index
+  get '/'do #'/' is a shortcut for the home page
+    "This is your home page"
+  end   
+  
+  get '/itslit' do 
+    erb:index
+  end 
+  
+  get '/hello/:bandfan/:band' do
+    @bandfanname = params[:bandfan] 
+    @bandname = params[:band]
+    "Hello #{@bandfanname}! We are, #{@bandname}"
   end
-
+  
+  get '/kpop' do
+    erb:kpop
+  end 
+  
+  post '/kpop' do
+    "My name is #{params[:name]}, and my favorite band is #{params[:favorite_band]}, and my favorite member is #{params[:favorite_member]}!"
+  end
 end
